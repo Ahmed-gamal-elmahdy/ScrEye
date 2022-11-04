503cabab6ebf8d6a830c59a3ef792e82132edbaa
Mostafa-Ibraheem-basheer <Mostafa.basheer00@eng-st.cu.edu.eg>
2022-11-04 16:12:47 +0200

    red

--- lib/app.dart
+++ lib/app.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
-            primarySwatch: Colors.blue,
+            primarySwatch: Colors.red,
           ),
           home: child,
         );
