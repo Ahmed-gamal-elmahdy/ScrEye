d46d5db66bfff22ad426b72d801ec5271e9e88a4
Mostafa-Ibraheem-basheer <Mostafa.basheer00@eng-st.cu.edu.eg>
2022-11-04 16:29:26 +0200

    intial commit

--- lib/app.dart
+++ lib/app.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
-            primarySwatch: Colors.red,
+            primarySwatch: Colors.blue,
           ),
           home: child,
         );
